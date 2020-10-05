95a0006b505a1dea624bd71ca4a4bf4c7e882f5a
Akshay.R.R <65683151+akshay1027@users.noreply.github.com>
2020-10-05 10:44:01 +0530

    Add files via upload

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Next.js app &nbsp;
+        <div className="footer__left">
+          <a href="https://github.com/snipcart/snipcart-nextjs">Github</a>
+        </div>
+      </p>
+    </footer>
+  );
+}
